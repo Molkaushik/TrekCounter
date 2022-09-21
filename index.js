dcc4f318b2e4ced43f664d71157c61d6937af3ef
Anmol <113883568+Molkaushik@users.noreply.github.com>
2022-09-21 16:16:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let countEl = document.getElementById("count-el");
+let trekCount = document.getElementById("trek-count");
+let count = 0;
+let totalTreks = 0;
+
+function increment(){
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save(){
+    totalTreks += count;
+    trekCount.textContent = "Total Treks: " + totalTreks;
+    count = 0;
+    countEl.textContent = 0;
+}
+
+function clearHist(){
+    trekCount.textContent = "Total Treks: 0";
+}
